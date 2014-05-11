5a12979b6f909ec754e7e651789aa38bb2fd79d0
leo9r <lezcano.leo@gmail.com>
2014-05-11 19:51:14 -0400

    v0.2

---
+++ plot3.R
@@ -0,0 +1,10 @@
+plot3 <- function()
+{
+  loaddata()  
+  plot(twodays[,c("fulltime", "Sub_metering_1")], type="l", 
+       ylab="Energy sub metering", xlab="", col="black")
+  lines(twodays[,c("fulltime", "Sub_metering_2")], col="red")
+  lines(twodays[,c("fulltime", "Sub_metering_3")], col="blue")  
+  legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), 
+         lty=c(1,1,1), lwd=c(2.5,2.5,2.5),col=c("black","red","blue"))
+}
